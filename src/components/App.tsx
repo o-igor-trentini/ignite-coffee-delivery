21fbc241da9bd953f5ca5f89d4c581c6c44323fb
Igor Trentini <igortrentini.2004@gmail.com>
2022-12-03 17:32:38 -0300

    fix: Corrige classes do componente de texto

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,13 @@
 import { FC } from 'react';
 import '../styles/main.css';
+import { Text } from './ui/Text';
 
 export const App: FC = () => {
-    return <div>Hello world!</div>;
+    return (
+        <div>
+            <Text size="lg" weight="regular">
+                Hello world
+            </Text>
+        </div>
+    );
 };
