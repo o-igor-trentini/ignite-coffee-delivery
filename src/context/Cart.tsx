6bf231e28cae0457f8ff6c9ecd1c322e544fcf7d
Igor Trentini <igortrentini.2004@gmail.com>
2022-12-17 21:58:33 -0300

    feat: Salvando carrinho em localstorage

--- src/context/Cart.tsx
+++ src/context/Cart.tsx
@@ -1,6 +1,6 @@
 import { Coffee } from '../pages/Home/components/CoffeeMenu/components/CoffeeList';
-import { createContext, FC, ReactNode, useReducer } from 'react';
-import { cartReducer } from '../reducer/reducer';
+import { createContext, FC, ReactNode, useEffect, useReducer } from 'react';
+import { cartReducer, CartState } from '../reducer/reducer';
 import { addOrderAction, removeOrderAction } from '../reducer/actions';
 
 export interface Order {
@@ -22,8 +22,23 @@ interface CartContextProviderProps {
 }
 
 export const CartContextProvider: FC<CartContextProviderProps> = ({ children }) => {
-    const [cartState, dispatch] = useReducer(cartReducer, { orders: [] });
+    const [cartState, dispatch] = useReducer(cartReducer, { orders: [] }, () => {
+        const storedStateAsJson = localStorage.getItem('@ignite-coffee-delivery:cart-state');
+
+        let storedCartState: CartState = { orders: [] };
+
+        if (storedStateAsJson) storedCartState = JSON.parse(storedStateAsJson);
+
+        return storedCartState;
+    });
+
     const { orders } = cartState;
+
+    useEffect(() => {
+        const stateJson = JSON.stringify(cartState);
+
+        localStorage.setItem('@ignite-coffee-delivery:cart-state', stateJson);
+    }, [cartState]);
 
     const addOrder = (order: Order): void => dispatch(addOrderAction(order));
 
