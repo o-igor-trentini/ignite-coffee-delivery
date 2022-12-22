b4be6fd6bd9699bbead29581fe3a101d93e48e64
Igor Trentini <igortrentini.2004@gmail.com>
2022-12-21 21:09:20 -0300

    wip: Pagina de sucesso

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,5 +1,25 @@
 import { FC } from 'react';
+import { Title } from '../../components/ui/Title';
+import { Text } from '../../components/ui/Text';
+import { Card } from '../../components/ui/Card';
+import styles from './index.module.css';
 
 export const Success: FC = () => {
-    return <div>success</div>;
+    return (
+        <div className={styles.successContainer}>
+            <div className={styles.successDescription}>
+                <Title size="lg" className={styles.successDescriptionTitle}>
+                    Uhu! Pedido confirmado
+                </Title>
+
+                <Text size="lg" weight="regular">
+                    Agora é só aguardar que logo o café chegará até você
+                </Text>
+            </div>
+
+            <Card border className={styles.successCard}>
+                aalksndlkasjdklasjdlkjsa
+            </Card>
+        </div>
+    );
 };
